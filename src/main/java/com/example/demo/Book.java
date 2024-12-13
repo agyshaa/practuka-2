12ab60de8feb918689a80179612f84c471a3740e
Vika <agyshii@gmail.com>
2024-12-13 18:36:09 +0200

    переробила першу форму

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,77 @@
+package com.example.demo;
+ public class Book {
+    private String title;
+    private String publishing;
+    private String year;
+    private String genre;
+    private String author;
+    private String views;
+    private String description;
+    public Book(String title, String publishing, String year, String genre, String author, String views, String description) {
+         this.title = title;
+         this.publishing = publishing;
+         this.year = year;
+         this.genre = genre;
+         this.author = author;
+         this.views = views;
+         this.description = description;
+     }
+
+     public String getTitle() {
+         return title;
+     }
+
+     public void setTitle(String title) {
+         this.title = title;
+     }
+
+     public String getPublishing() {
+         return publishing;
+     }
+
+     public void setPublishing(String publishing) {
+         this.publishing = publishing;
+
+     }
+
+     public String getYear() {
+         return year;
+     }
+
+     public void setYear(String year) {
+         this.year = year;
+     }
+
+     public String getGenre() {
+         return genre;
+     }
+
+     public void setGenre(String genre) {
+         this.genre = genre;
+     }
+
+     public String getAuthor() {
+         return author;
+     }
+
+     public void setAuthor(String author) {
+         this.author = author;
+     }
+
+     public String getViews() {
+         return views;
+     }
+
+     public void setViews(String views) {
+         this.views = views;
+     }
+
+     public String getDescription() {
+         return description;
+     }
+
+     public void setDescription(String description) {
+         this.description = description;
+     }
+ }
+
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,10 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+Book book=new Book ("Вище неба", "Dbl", "214323","eggvdxv", "kgemmbias", "2140124", "jsdfwufhskjbcnjdsbfhjsjkfnkzdslvml");
 
     @GetMapping("/")
     public String home(Model model) {
-//        model.addAttribute("message", "Welcome to Spring Boot with Thymeleaf!");
+        String title = book.getTitle();
+        String publishing=book.getPublishing();
+        String year=book.getYear();
+        String genre=book.getGenre();
+        String author=book.getAuthor();
+        String views=book.getViews();
+        String description=book.getDescription();
+        model.addAttribute("title", title);
+        model.addAttribute("publishing", publishing);
+        model.addAttribute("year", year);
+        model.addAttribute("genre", genre);
+        model.addAttribute("author", author);
+        model.addAttribute("views", views);
+        model.addAttribute("description", description);
         return "index";
     }
+
 }
